e7c3932ce54d9751751eaa75f36e36288cae3757
Happy Harsh <ranjanharsh381@gmail.com>
2026-05-10 04:10:37 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Chatbox from "./components/Chatbox.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
