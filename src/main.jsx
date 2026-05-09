0a4939f54ae5bdebf95a75fbfce5611fa2fb6d9a
Happy Harsh <ranjanharsh381@gmail.com>
2026-05-10 04:08:30 +0530

    final update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/chats" element={<Chatbox />} />
+			</Routes>
+		</BrowserRouter>
+		<App />
+	</StrictMode>,
+);
